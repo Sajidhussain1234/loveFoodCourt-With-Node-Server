38183291f8bc9797b54baa599efc2f9509bd0911
Sajid Hussain <sajidmcsbwn@gmail.com>
2023-04-30 15:37:22 +0500

    connected with db and write schemas

--- server/server.js
+++ server/server.js
@@ -1,12 +1,69 @@
-// first of all install express server then include it your project
-// command is: npm install express --save
-const express = require('express');
-const app = express(); 
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-  
-  app.listen(6000, () => {
-    console.log('Server started on port 6000');
-  });
+const port = 6000;
+const mongoURI =  "mongodb+srv://sajid:mAFzg1djvhlQF1kI@cluster0.davbvrm.mongodb.net/kfcDb?retryWrites=true&w=majority";
+
+mongoose.connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Sajid");
+});
+
+// api for category
+app.get('/category', async (req, res) => {
+  try {
+    const category = await Category.find();
+    res.json(category);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// api for product
+app.get('/product', async (req, res) => {
+  try {
+    const product = await Product.find();
+    res.json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server started successfully on port number ${port}`);
+});
+
+// Schema for category collection 
+const categorySchema = new mongoose.Schema({
+  name: String,
+  imgUrl: String,
+});
+const Category = mongoose.model("category", categorySchema);
+
+
+// Schema for product collection 
+const productSchema = new mongoose.Schema({
+  name: String,
+  imgUrl: String,
+  description: String,
+  price: Number,
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Category,
+  },
+});
+const Product = mongoose.model("product", productSchema);
+
+
+
+
+
+
+
