681678827f18453ac9e4c8e1e9fe6bd0b6641559
Sajid Hussain <sajidmcsbwn@gmail.com>
2023-04-30 18:34:01 +0500

    Made changes in schemas

--- server/server.js
+++ server/server.js
@@ -1,51 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require('morgan');
+const cors = require('cors');
 
-const port = 6000;
-const mongoURI =  "mongodb+srv://sajid:mAFzg1djvhlQF1kI@cluster0.davbvrm.mongodb.net/kfcDb?retryWrites=true&w=majority";
-
-mongoose.connect(mongoURI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Failed to connect to MongoDB", err));
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello, Sajid");
-});
-
-// api for category
-app.get('/category', async (req, res) => {
-  try {
-    const category = await Category.find();
-    res.json(category);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-// api for product
-app.get('/product', async (req, res) => {
-  try {
-    const product = await Product.find();
-    res.json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server started successfully on port number ${port}`);
-});
+// ================= Start Database ======================
 
 // Schema for category collection 
 const categorySchema = new mongoose.Schema({
   name: String,
   imgUrl: String,
 });
-const Category = mongoose.model("category", categorySchema);
+const Category = mongoose.model("Category", categorySchema);
 
 
 // Schema for product collection 
@@ -59,11 +24,46 @@ const productSchema = new mongoose.Schema({
     ref: Category,
   },
 });
-const Product = mongoose.model("product", productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 
+const port = 6000;
+const mongoURI = "mongodb+srv://sajid:CwyHs0PUSopsDMF1@cluster0.davbvrm.mongodb.net/kfcDb?retryWrites=true&w=majority";
 
+mongoose.connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
 
+// ================= End Database ======================
 
+const app = express();
+app.use(morgan('tiny'));
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Hello, Sajid");
+});
 
+// api for category
+app.get('/categories', async (req, res) => {
+  try {
+    const categories = await Category.find({});
+    res.json(categories);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// api for product
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server started successfully on port number ${port}`);
+});
